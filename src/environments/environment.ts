336806ecf685dfa8635bb867509b85e758d3bb58
Roshankavinda <45584797+lahiruroot@users.noreply.github.com>
2021-05-31 00:05:40 +0530

    Fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase:{
     apiKey: "AIzaSyBQRMFFyNa2xx1KvZYTO8kJ5K_lzfuiIro",
     authDomain: "tapaware-63b30.firebaseapp.com",
