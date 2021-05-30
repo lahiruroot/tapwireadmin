9be804427687fb1a2c8ea17d1a6ba73c6b5c0ad6
Roshankavinda <45584797+lahiruroot@users.noreply.github.com>
2021-05-31 00:12:38 +0530

    Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBQRMFFyNa2xx1KvZYTO8kJ5K_lzfuiIro",
+    authDomain: "tapaware-63b30.firebaseapp.com",
+    projectId: "tapaware-63b30",
+    storageBucket: "tapaware-63b30.appspot.com",
+    messagingSenderId: "736101288677",
+    appId: "1:736101288677:web:f8419331f0b58cce4ca20c",
+    measurementId: "G-BVGW73N1GL"
+  }
 };
